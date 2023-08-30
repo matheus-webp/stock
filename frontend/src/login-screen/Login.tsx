e6a3595b66f45e1f305827b64156a5c594611bd9
matheus-webp <matheus.webp@gmail.com>
2023-08-30 20:51:37 -0300

    feat: add login and signup

--- frontend/src/login-screen/Login.tsx
+++ frontend/src/login-screen/Login.tsx
@@ -2,11 +2,20 @@ import React from "react";
 import axios from "axios";
 import UnauthorizedError from "./components/error/unauthorized-error";
 import "./login.css";
+import { CustomInput } from "../components/input/custom-input";
 
-function Login() {
+export function Login() {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [error, setError] = React.useState(false);
+
+  const handleEmailCHange = (e: any) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: any) => {
+    setPassword(e.target.value);
+  };
 
   const handleFormSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -23,47 +32,53 @@ function Login() {
     }
   };
 
+  const loginDemoAccount = async (event: React.FormEvent) => {
+    event.preventDefault();
+    await axios.post(`http://localhost:3000/login`, {
+      email: "demo@demo.com",
+      password: "123456",
+    });
+  };
+
   return (
-    <div id="login">
-      {error && <UnauthorizedError />}
-      <div id="loginTitle">Login</div>
-      <form onSubmit={handleFormSubmit}>
-        <div className="labelLayout">
-          <label htmlFor="emailInput" className="inputTitle">
-            Email
-          </label>
-          <div>
-            <input
-              type="text"
-              id="emailInput"
-              className="inputText"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
+    <div id="main">
+      <div id="login">
+        {error && <UnauthorizedError />}
+        <div id="loginTitle">Login</div>
+        <form onSubmit={handleFormSubmit}>
+          <CustomInput
+            value={email}
+            type="text"
+            label="Email"
+            onChange={handleEmailCHange}
+          />
+          <CustomInput
+            value={password}
+            type="password"
+            label="Senha"
+            onChange={handlePasswordChange}
+          />
+          <button type="submit" className="enter-button">
+            Entrar
+          </button>
+          <p className="footer-text">
+            Não tem uma conta?{" "}
+            <a href="http://localhost:5173/signup">Criar conta</a>
+          </p>
+          <div className="footer">
+            <div className="line"></div>
+            <p className="account-demo">Ou</p>
+            <div className="line"></div>
           </div>
-        </div>
-        <div className="labelLayout">
-          <label htmlFor="passwordInput" className="inputTitle">
-            Senha
-          </label>
-          <div>
-            <input
-              type="password"
-              id="passwordInput"
-              className="inputText"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-          </div>
-          <button type="submit"> Entrar</button>
-        </div>
-      </form>
+          <button
+            type="submit"
+            className="demo-button"
+            onClick={loginDemoAccount}
+          >
+            Entrar com conta demo
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
-
-export default Login;
